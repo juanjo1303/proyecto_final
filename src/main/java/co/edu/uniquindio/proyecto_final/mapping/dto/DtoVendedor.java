6b087861293ba2ccd294281701cbd2e6fc6cf246
Sebastian <sebastian.quinterof@uqvirtual.edu.co>
2024-11-08 09:10:28 -0500

    Avance registroVendedor

---
+++ src/main/java/co/edu/uniquindio/proyecto_final/mapping/dto/DtoVendedor.java
@@ -0,0 +1,4 @@
+package co.edu.uniquindio.proyecto_final.mapping.dto;
+
+public record DtoVendedor(String nombre, String apellido, String cedula, String direccion, String usuario, String contrasena) {
+}
--- src/main/java/co/edu/uniquindio/proyecto_final/service/ICrudVendedor.java
+++ src/main/java/co/edu/uniquindio/proyecto_final/service/ICrudVendedor.java
@@ -4,7 +4,7 @@ import co.edu.uniquindio.proyecto_final.model.builder.VendedorBuilder;
 import java.util.LinkedList;
 
 public interface ICrudVendedor {
-    boolean crearVendedor(String cedula,VendedorBuilder vendedorBuilder);
+    boolean crearVendedor(Vendedor newVendedor);
     boolean eliminarVendedor(String cedula);
     boolean actualizarProducto(String cedula, VendedorBuilder vendedorBuilder);
     Vendedor getVendedorCrud(String cedula);
