c1eba53a55c68cc1985bc834bd59637d36dafafc
Sebastian <sebastian.quinterof@uqvirtual.edu.co>
2024-11-08 09:23:30 -0500

    Arreglo modelFactory implement singleton

--- src/main/java/co/edu/uniquindio/proyecto_final/factory/ModelFactory.java
+++ src/main/java/co/edu/uniquindio/proyecto_final/factory/ModelFactory.java
@@ -1,4 +1,28 @@
 package co.edu.uniquindio.proyecto_final.factory;
 
+import co.edu.uniquindio.proyecto_final.mapping.mappers.MarketPlaceMappingImpl;
+import co.edu.uniquindio.proyecto_final.model.MarketPlace;
+
 public class ModelFactory {
+    private static ModelFactory instance;
+    MarketPlace marketPlace;
+    MarketPlaceMappingImpl mapper;
+
+    public static ModelFactory getInstance() {
+        if (instance == null) {
+            instance = new ModelFactory();
+        }
+        return instance;
+    }
+
+    private ModelFactory() {
+        mapper = new MarketPlaceMappingImpl();
+        marketPlace = inicializarDatos();
+    }
+
+    private static MarketPlace inicializarDatos() {
+        MarketPlace marketPlace = new MarketPlace("abc");
+        return marketPlace;
+    }
+
 }
